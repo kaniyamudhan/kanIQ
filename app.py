dfb8caf3270a2217269aefc25ff495635d5ceef1
KANIYAMUDHAN Y <112994943+kaniyamudhan@users.noreply.github.com>
2024-04-06 10:05:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,13 @@ import streamlit as st
 import textwrap
 import google.generativeai as genai
 
-# Load environment variables from .env file
 load_dotenv()
-
-# Configure API key
 api_key = os.getenv("GOOGLE_API_KEY")
 if api_key is None:
     st.error("Error: API key not found. Please make sure to set the GOOGLE_API_KEY environment variable.")
     st.stop()
 
 genai.configure(api_key=api_key)
-
-# Function to load OpenAI model and get responses
 def get_gemini_response(question):
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(question)
@@ -25,17 +20,13 @@ def get_gemini_response(question):
         st.error("Error: Failed to generate response. Please try again with a different input.")
         return None
 
-# Initialize Streamlit app
-st.set_page_config(page_title="kaxz")
-st.header("kaxz ask")
+st.set_page_config(page_title="KanIQ 😉",page_icon="🤖")
+st.header("KanIQ 😉")
 
-# Input box for the question
 input_text = st.text_input("Input:", key="input")
 
-# Button to submit the question
 submit_button = st.button("submit")
 
-# If the submit button is clicked
 if submit_button:
     response = get_gemini_response(input_text)
     if response is not None:
